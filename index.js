0314127d5faa0cc899588a54a2437fd0122cdeac
Rahman Fadhil <rhmnfadhil@gmail.com>
2018-04-12 13:55:53 +0700

    Enable CORS

--- index.js
+++ index.js
@@ -1,8 +1,10 @@
 const express = require('express')
 const bodyparser = require('body-parser')
+const cors = require('cors')
 
 const app = express()
 
+app.use(cors())
 app.use(bodyparser.urlencoded({ extended: false }))
 app.use(bodyparser.json())
 
